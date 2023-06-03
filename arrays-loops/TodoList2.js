4ef085cb0c55f6446205fbf4f6c89f515edfb536
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-03 16:58:43 +0430

    Create TodoList2.js

---
+++ arrays-loops/TodoList2.js
@@ -0,0 +1,15 @@
+const todoList=['make dinner','wash dishes'];
+let todoListHtml='';
+for(let i=0;i < todoList.length;i++){
+    const todo=todoList[i];
+    const html=`<p>${todo}</p>`;
+    todoListHtml+=html;
+}
+    console.log(todoListHtml);
+function addTodo(){
+    const inputElement=document.querySelector('.js-name-input');
+    const name=inputElement.value;
+    todoList.push(name);
+    console.log(todoList);
+    inputElement.value='';
+}
