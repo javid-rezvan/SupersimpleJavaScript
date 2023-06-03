7f368a35e1eeed26341b2c6ec45556650c4479ed
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-03 18:16:22 +0430

    Update TodoList2.js

--- arrays-loops/TodoList2.js
+++ arrays-loops/TodoList2.js
@@ -1,15 +1,23 @@
 const todoList=['make dinner','wash dishes'];
-let todoListHtml='';
-for(let i=0;i < todoList.length;i++){
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHtml='';
+    for(let i=0;i < todoList.length;i++){
     const todo=todoList[i];
     const html=`<p>${todo}</p>`;
     todoListHtml+=html;
-}
+    }
     console.log(todoListHtml);
+    document.querySelector('.js-todo-list').innerHTML=todoListHtml;
+
+}
+
 function addTodo(){
     const inputElement=document.querySelector('.js-name-input');
     const name=inputElement.value;
     todoList.push(name);
     console.log(todoList);
     inputElement.value='';
+    renderTodoList();
 }
