5751bb87a3d8861822130eaf1df4ee89877178f8
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-03 19:39:40 +0430

    Update TodoList3.js

--- arrays-loops/TodoList3.js
+++ arrays-loops/TodoList3.js
@@ -17,10 +17,13 @@ function renderTodoList(){
     // const dueDate=todoObject.dueDate;
     const {name,dueDate} =todoObject
 
-    const html=`<p>${name} ${dueDate} <button onclick="
+    const html=`
+    <div>${name}</div> 
+    <div>${dueDate}</div> 
+    <button onclick="
     todoList.splice(${i},1)
     renderTodoList(); 
-    ">Delete</button></p>`;
+    ">Delete</button>`;
     todoListHtml+=html;
     }
     document.querySelector('.js-todo-list').innerHTML=todoListHtml;
