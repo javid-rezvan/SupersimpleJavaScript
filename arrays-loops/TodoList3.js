b7b3bb480f30eb23ee5d58820cbb788b4173af86
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-03 18:35:46 +0430

    Create TodoList3.js

---
+++ arrays-loops/TodoList3.js
@@ -0,0 +1,26 @@
+const todoList=['make dinner','wash dishes'];
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHtml='';
+    for(let i=0;i < todoList.length;i++){
+    const todo=todoList[i];
+    const html=`<p>${todo} <button onclick="
+    todoList.splice(${i},1)
+    renderTodoList(); 
+    ">Delete</button></p>`;
+    todoListHtml+=html;
+    }
+    console.log(todoListHtml);
+    document.querySelector('.js-todo-list').innerHTML=todoListHtml;
+
+}
+
+function addTodo(){
+    const inputElement=document.querySelector('.js-name-input');
+    const name=inputElement.value;
+    todoList.push(name);
+    console.log(todoList);
+    inputElement.value='';
+    renderTodoList();
+}
