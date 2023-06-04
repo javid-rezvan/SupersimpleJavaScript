12d097d0a59bb8e2434b48ccc081d625f30826e4
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-06-04 17:22:08 +0430

    Create TodoList4.js

---
+++ arrays-loops/scripts/TodoList4.js
@@ -0,0 +1,44 @@
+const todoList=[
+    {
+     name:'make dinner',
+     dueDate:'2022-12-22'
+    },{
+     name:'wash dishes',
+     dueDate:'2022-12-22'
+    }
+];
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHtml='';
+    todoList.forEach(function(todoObject,index){
+        const {name,dueDate} =todoObject
+    
+        const html=`
+        <div>${name}</div> 
+        <div>${dueDate}</div> 
+        <button class="delete-todo-button" onclick="
+        todoList.splice(${index},1)
+        renderTodoList(); 
+        ">Delete</button>`;
+        todoListHtml+=html;
+
+    });
+    document.querySelector('.js-todo-list').innerHTML=todoListHtml;
+
+}
+
+function addTodo(){
+    const inputElement=document.querySelector('.js-name-input');
+    const dateInputElement=document.querySelector('.js-due-date-input');
+    const name=inputElement.value;
+    const dueDate=dateInputElement.value;
+    todoList.push({
+        // name:name,
+        // dueDate:dueDate
+        name,  //this is shortcut for name:name and dueDate=dueDate
+        dueDate 
+    });
+    inputElement.value='';
+    renderTodoList();
+}
