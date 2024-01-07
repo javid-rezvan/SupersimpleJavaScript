104f9dab5eea6d0c9a2b4ae1873f63a21645f8bf
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2024-01-07 16:09:38 +0330

    Create Todo-List-2.js

---
+++ Requirements/My Practice with Notation/scripts/Todo-List-2.js
@@ -0,0 +1,49 @@
+const todolist=[
+  {
+    name:'wash dishes',
+    dueDate:'2024-01-10'
+  },
+  {
+    name:'make dinner',
+    dueDate:'2024-01-12'
+  },
+];
+renderTodoList();
+
+function addTodoList(){
+  const inputElement=document.querySelector('.js-name-input');
+  const dateElement=document.querySelector('.js-due-input');
+  const name=inputElement.value;
+  const dueDate=dateElement.value;
+
+  todolist.push({
+   /* name:name,
+    dueDate:dueDate
+
+    we use shortcut
+    */
+    name,
+    dueDate
+  });
+  inputElement.value='';
+  renderTodoList();
+}
+
+function renderTodoList(){
+  let todoListHtml='';
+  for(let i=0;i<todolist.length;i++){
+      const todoObject=todolist[i];
+     /* const name=todoObject.name;
+      const dueDate=todoObject.dueDate;
+
+       we use shortcut
+      */
+      const {name,dueDate}=todoObject;
+
+      const html=`<p>${name}  ${dueDate} <button onclick="todolist.splice(${i},1); renderTodoList();"
+      >Delete</button></p>`;
+      todoListHtml += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML=todoListHtml;
+
+}
