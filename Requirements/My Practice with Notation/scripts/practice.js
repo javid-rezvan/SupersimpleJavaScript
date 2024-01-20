a9a3d83c213a7fa8a443620831c13adac9471f22
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2024-01-20 16:33:30 +0330

    Create practice.js

---
+++ Requirements/My Practice with Notation/scripts/practice.js
@@ -0,0 +1,60 @@
+let score ={
+  Wins:0,
+  Loses:0,
+  Ties:0
+}
+document.querySelector('.js-result').innerHTML=`Wins:${score.Wins} Losses:${score.Losses} Ties:${score.Ties}`
+
+function pickComputerMove(){
+  const randomNumber=Math.random();
+  const computerMove;
+  if(randomNumber >=0 && randomNumber <1/3){
+    computerMove='rock';
+  }else if(randomNumber >=1/3 && randomNumber <= 2/3){
+    computerMove='paper';
+  }else{
+    computerMove='scissor';
+  }
+  return computerMove;
+}
+
+function playGame(playerMove){
+  const computerMove=pickComputerMove();
+  let result='';
+   if(playerMove === 'rock'){
+    if(computerMove === 'rock'){
+      result='Tie';
+    }else if(computerMove === 'paper'){
+      result='you lose';
+    }else{
+      result='You win';
+    }
+   }else if(playerMove === 'paper'){
+    if(computerMove == 'rock'){
+      result='You win';
+    }else if(computerMove === 'paper'){
+      result='Tie';
+    }
+   }else{
+      result='You lose';
+   }else{
+       if(computerMove === 'rock'){
+        result='you lose';
+       }else if(computerMove === 'paper'){
+         result='You win';
+       }else {
+         result='Tie';
+       }
+       updateScore(result);
+   }
+
+}
+ function updateScore(result){
+  if (result === 'You win'){
+    score.Wins+=1;
+  }else if(result == 'You lose'){
+    score.Loses+=1;
+  }else {
+    score.Ties+=1;
+  }
+ }
